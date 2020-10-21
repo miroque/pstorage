c90d481f1664ff825919c52f65f45e4bfcf64813
Panov Aleksander <i.miroque@gmail.com>
2020-10-21 22:10:53 +0700

    Initial commit

---
+++ src/main/java/ru/miroque/pstorage/PstorageApplication.java
@@ -0,0 +1,13 @@
+package ru.miroque.pstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PstorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PstorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/miroque/pstorage/PstorageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.miroque.pstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PstorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
